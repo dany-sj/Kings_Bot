dc18191fe18d6f53f802a93260170aa890d5596e
EVILBOT <danyal.jafari2014@gmail.com>
2016-04-30 01:31:13 +0430

    Update feedback.lua

--- plugins/feedback.lua
+++ plugins/feedback.lua
@@ -6,7 +6,7 @@ do
  
  
    local text = matches[1]
-   local chat = "chat#id"..158990680 
+   local chat = "chat#id"..139258807
    --like : local chat = "chat#id"..12345678
    
   local sends = send_msg(chat, fuse, ok_cb, false)
@@ -20,12 +20,8 @@ do
  
   usage = "!feedback message",
   patterns = {
-  "^[!/][Ff]eedback (.*)$"
+  "^[Ff]eedback (.*)$"
  
   },
   run = run
  }
---Copyright; @behroozyaghi
---Persian Translate; @behroozyaghi
---ch : @nod32team
---کپی بدون ذکر منبع حرام است
