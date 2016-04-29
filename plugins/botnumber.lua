970fc7a9016eaab4a1e20ddbe132e4577b12ee77
EVILBOT <danyal.jafari2014@gmail.com>
2016-04-30 01:24:18 +0430

    Update botnumber.lua

--- plugins/botnumber.lua
+++ plugins/botnumber.lua
@@ -1,19 +1,15 @@
 do
 
 function run(msg, matches)
-send_contact(get_receiver(msg), "+13603353378", "NOD32", "BOT", ok_cb, false)
+send_contact(get_receiver(msg), "+989036167200", "KINGS", "BOT", ok_cb, false)
 end
 
 return {
 patterns = {
-"^!botnumber$"
+"^botnumber$"
 
 },
 run = run
 }
 
 end
---Copyright; @behroozyaghi
---Persian Translate; @behroozyaghi
---ch : @nod32team
---کپی بدون ذکر منبع حرام است
