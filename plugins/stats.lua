90072e7499eb635e40de32ab9807357493218331
EVILBOT <danyal.jafari2014@gmail.com>
2016-04-30 01:44:07 +0430

    Update stats.lua

--- plugins/stats.lua
+++ plugins/stats.lua
@@ -91,7 +91,7 @@ local function bot_stats()
   return text
 end
 local function run(msg, matches)
-  if matches[1]:lower() == 'nod32' then -- Put everything you like :)
+  if matches[1]:lower() == 'kings' then -- Put everything you like :)
     local about = _config.about_text
     local name = user_print_name(msg.from)
     savelog(msg.to.id, name.." ["..msg.from.id.."] used /nod32 ")
@@ -120,7 +120,7 @@ local function run(msg, matches)
         return
       end
     end
-    if matches[2] == "nod32" then -- Put everything you like :)
+    if matches[2] == "kings" then -- Put everything you like :)
       if not is_admin(msg) then
         return "For admins only !"
       else
@@ -138,18 +138,13 @@ local function run(msg, matches)
 end
 return {
   patterns = {
-    "^[!/]([Ss]tats)$",
-    "^[!/]([Ss]tatslist)$",
-    "^[!/]([Ss]tats) (group) (%d+)",
-    "^[!/]([Ss]tats) (nod32)",-- Put everything you like :)
-		"^[!/]([Nn]od32)"-- Put everything you like :)
+    "^([Ss]tats)$",
+    "^([Ss]tatslist)$",
+    "^([Ss]tats) (group) (%d+)",
+    "^([Ss]tats) (kings)",-- Put everything you like :)
+		"^([Kk]ings)"-- Put everything you like :)
     }, 
   run = run
 }
 
 end
-
---Copyright and edit; @behroozyaghi
---Persian Translate; @behroozyaghi
---ch : @nod32team
---کپی بدون ذکر منبع حرام است
